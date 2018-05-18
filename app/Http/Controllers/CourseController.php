925527b8df6b78e2e4cdc9cc2c2d35fc42927783
Dani Heras <danielheras@bewanted.com>
2018-05-18 13:04:34 +0200

    Code cleaned

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -85,46 +85,4 @@ class CourseController extends Controller{
       return response()->json(["Message" => 'The course has been modified'], 201);
     }
 
-    function coursesId(Request $request, $id){
-      $courses = DB::select("select * from courses where id in (select course_id from users_courses where user_id = ". $id .");");
-      return response()->json($courses, 200);
-    }
-
-    // function course(Request $request, $id){
-    //   $course = DB::select("select * from goodle.courses where id = ". $id .";");
-    //   return response()->json($course, 200);
-    // }
-
-    function course(Request $request, $id_course){
-      $id = $request->json()->get('user_id');
-      $user_courses = DB::select("select id from courses where id in (select course_id from users_courses where user_id = ". $id .");");
-      $id_course = intval($id_course);
-      $aray = [];
-      for ($i=0; $i < count($user_courses); $i++) {
-        $array[] = $user_courses[$i]->id;
-      }
-      if (in_array($id_course, $array)) {
-        $course = DB::select("select * from courses where id = ". $id_course .";");
-        return response()->json($course, 200);
-      }else {
-        return response()->json(["error" => "This user is not registered in the required course"], 400);
-      }
-    }
-
-
-    function addUserToCourse(Request $request) {
-      $user_course = UserCourse::create([
-        'user_id' => $request->json()->get('user_id'),
-        'course_id' => $request->json()->get('course_id')
-      ]);
-      return response()->json($user_course, 201);
-    }
-
-    function unsubscribeCourse(Request $request) {
-      $user_id = $request->json()->get('user_id');
-      $course_id = $request->json()->get('course_id');
-      $delete = DB::select('delete from users_courses where user_id = ' . $user_id . ' and course_id = '. $course_id .';');
-      return response()->json($delete, 200);
-    }
-
 }
